e8341f019a97d05e1811c74e0c702f6213899deb
Mohammad Talebi-Kalaleh <talebika@ualberta.ca>
2025-04-05 12:28:38 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -115,7 +115,6 @@ def generate_plot_1d(obj_func, x_opt):
     fig.update_layout(title='1D Objective Function', xaxis_title='x', yaxis_title='f(x)')
     return fig
 
-
 def generate_plot_2d_contour(obj_func, x_opt):
     """
     Generate a 2D contour plot for a function of two variables, x & y.
@@ -123,21 +122,19 @@ def generate_plot_2d_contour(obj_func, x_opt):
     """
     import numpy as np
     import plotly.graph_objects as go
-    
+
     # Create a grid
     x_vals = np.linspace(-10, 10, 50)
     y_vals = np.linspace(-10, 10, 50)
     X, Y = np.meshgrid(x_vals, y_vals)
 
-    # Make sure Z is a float array
     Z = np.zeros_like(X, dtype=float)
 
-    # Safely evaluate the function on each grid point
+    # Evaluate the function on each grid point, handling any exceptions
     for i in range(X.shape[0]):
         for j in range(X.shape[1]):
             try:
                 val = obj_func([X[i, j], Y[i, j]])
-                # If the function returns something invalid or complex, catch it:
                 if np.iscomplex(val):
                     Z[i, j] = np.nan
                 else:
@@ -146,6 +143,7 @@ def generate_plot_2d_contour(obj_func, x_opt):
                 Z[i, j] = np.nan
 
     # Build the contour figure
+    # NOTE: 'smoothing' must be between 0 and 1, so we'll set it to 0 or 0.8 if needed.
     fig = go.Figure(
         data=[
             go.Contour(
@@ -153,17 +151,17 @@ def generate_plot_2d_contour(obj_func, x_opt):
                 x=x_vals,
                 y=y_vals,
                 colorscale="Viridis",
-                # We configure "contours" via a dictionary:
                 contours=dict(
-                    coloring="heatmap",   # or "lines", etc.
+                    coloring="heatmap",
                     showlines=False,
-                    smoothing=1.3
+                    # If you want some smoothing, set a value between 0 and 1:
+                    smoothing=0.0
                 )
             )
         ]
     )
 
-    # Mark the optimum on the contour
+    # Plot the optimum point
     fig.add_trace(
         go.Scatter(
             x=[x_opt[0]],
@@ -181,7 +179,6 @@ def generate_plot_2d_contour(obj_func, x_opt):
     )
 
     return fig
-
 
 def generate_plot_2d_surface(obj_func, x_opt):
     """
